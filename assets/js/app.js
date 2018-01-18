4f171297bce0075bbca66553fb79e58c62ec8c66
Philippe Lewin <philippe.lewin@gmail.com>
2018-01-19 00:15:19 +0100

    Feature input password toggle visibility

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 var $ = require('jquery');
-
+require('hideshowpassword');
 require('bootstrap-sass');
 
 $(document).ready(function(){
@@ -8,5 +8,13 @@ $(document).ready(function(){
         trigger: 'hover',
         placement: 'bottom',
         container: 'body' // Allows the popover to be larger than the menu button
+    });
+
+    // toggle password visibility
+    $('.password + .glyphicon').on('click', function() {
+        // toggle our classes for the eye icon
+        $(this).toggleClass('glyphicon-eye-close').toggleClass('glyphicon-eye-open');
+        // activate the hideShowPassword plugin
+        $(this).prev('.password').togglePassword();
     });
 });
