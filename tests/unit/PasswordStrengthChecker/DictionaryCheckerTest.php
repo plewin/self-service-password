7890dfe1faa73b845368d0e3eb77886e72c70f41
Philippe Lewin <philippe.lewin@gmail.com>
2018-07-27 00:23:23 +0200

    Fix failing test DictionaryCheckerTest

--- tests/unit/PasswordStrengthChecker/DictionaryCheckerTest.php
+++ tests/unit/PasswordStrengthChecker/DictionaryCheckerTest.php
@@ -3,6 +3,8 @@
 namespace App\Tests\Unit\PasswordStrengthChecker;
 
 use App\PasswordStrengthChecker\DictionaryChecker;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\RouterInterface;
 
 /**
  * Class DictionaryCheckerTest
@@ -14,7 +16,15 @@ class DictionaryCheckerTest extends \PHPUnit_Framework_TestCase
      */
     public function testCheckPasswordStrength()
     {
-        $checker = new DictionaryChecker(['dirs' => [__DIR__.'/../../../var/dictionaries']]);
+        $mockRouter = $this->getMock(RouterInterface::class);
+        $mockRouter->expects($this->any())->method('generate');
+
+        $requestStack = new RequestStack();
+
+        $checker = new DictionaryChecker([
+            'dirs' => [__DIR__.'/../../../var/dictionaries'],
+            'enable' => true,
+        ], $requestStack, $mockRouter);
 
         $this->assertSame(['indictionary'], $checker->evaluate("123456"));
         $this->assertSame([], $checker->evaluate("P455w0rd"));
